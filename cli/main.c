9dff6a55c3d9d87f7017b3a7ce0827a8e5c06405
Alexander Drozdov <snosme@gmail.com>
2020-08-22 23:22:29 +0300

    cli

--- cli/main.c
+++ cli/main.c
@@ -6,9 +6,10 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <mtsdata.h>
+#include <unistd.h>
 
 void mtsd_error(mtsd_error_source src, int error) {
-  printf("mtsd_error\n");
+  printf("mtsd_error %d, %d\n", src, error);
 }
 void mtsd_error_msg(mtsd_error_source src, int error, char *msg) {
   printf("mtsd_error_msg: %s\n", msg);
@@ -45,8 +46,8 @@ void hexDump (const char * desc, const void * addr, const int len) {
 
 void readfile(const char* filename, uint8_t** content, size_t* size) {  
   FILE* file = fopen(filename, "rb");
-  if (file == NULL) {
-    printf("Cannot open file  '%s'.\n", filename);
+  if (!file) {
+    printf("Cannot open file '%s'.\n", filename);
     exit(1);
   }
 
@@ -66,6 +67,21 @@ void readfile(const char* filename, uint8_t** content, size_t* size) {
   int read = fread(*content, sizeof(uint8_t), *size, file);
   if (read != *size) {
     printf("Fatal error while reading file.\n");
+    exit(1);
+  }
+
+  fclose(file);
+}
+
+void write_to_file(const char* filename, uint8_t* data, size_t size) {
+  FILE* file = fopen(filename, "wb");
+  if (!file) {
+    printf("Cannot open file '%s'.\n", filename);
+    exit(1);
+  }
+
+  if (fwrite(data, sizeof(uint8_t), size, file) != size) {
+    printf("Fatal error while writing to file.\n");
     exit(1);
   }
 
@@ -96,42 +112,118 @@ int read_handler(void *data, uint8_t *buffer, size_t size, size_t *size_read) {
   return 1;
 }
 
-int main() {
-  cli_reader_state reader = { .read = 0 };
-  readfile("./data.mtsd", &reader.content, &reader.size);
-  mtsd_document doc;
-  if (!mtsd_parse(read_handler, &reader, &doc)) {
-    printf("Cannot parse file\n");
+static size_t records_count(mtsd_document* doc);
+
+int main(int argc, char **argv) {
+  if (argc == 1 || !strcmp(argv[1], "--help")) {
+    printf(
+      "MyTopSecret-Data v1.0 (c) Alexander Drozdov\n"
+      "----------------\n"
+      "     __ ____    ____  _________   ______   ______\n"
+      "    / /|_   \\  /   _||  _   _  |.' ____ \\ |_   _ `.\n"
+      "   / /   |   \\/   |  |_/ | | \\_|| (___ \\_|  | | `. \\\n"
+      "  / /    | |\\  /| |      | |     _.____`.   | |  | |\n"
+      " / /    _| |_\\/_| |_    _| |_   | \\____) | _| |_.' /_\n"
+      "/_/    |_____||_____|  |_____|   \\______.'|______.'(_)\n\n"
+      "USAGE:\n\n"
+      "  mtsd -e plaintext.mtsd -o encrypted.bin\n"
+      "  mtsd encrypted.bin\n\n"
+      "OPTIONS:\n\n"
+      "  -e <file>        Encrypt file\n"
+      "  -d <file>        Decrypt file\n"
+      "  -o <file>        Save output to file. If ommited prints to stdout\n"
+      "  --help           Help\n"
+    );
     exit(1);
   }
-  free(reader.content);
 
-  size_t raw_size;
-  uint8_t* out = malloc(MTSD_HEADER_SIZE + MTSD_PAYLOAD_MAX_SIZE);
-  if (!mtsd_encode(&doc, out + MTSD_HEADER_SIZE, &raw_size)) {
-    printf("Cannot encode file to binary format\n");
+  int encrypt = 0;
+  int decrypt = 0;
+  char* input_filename = NULL;
+  char* output_filename = NULL;
+
+  for (int i = 1; i < argc; i += 1) {
+    if (!strcmp(argv[i], "-e")) {
+      encrypt = 1;
+    }
+    else if (!strcmp(argv[i], "-d")) {
+      decrypt = 1;
+    }
+    else if (!strcmp(argv[i], "-o")) {
+      if ((i + 1) >= argc) { printf("Missing value for '-o' option.\n"); exit(1); }
+      output_filename = argv[++i];
+    }
+    else {
+      input_filename = argv[i];
+    }
+  }
+
+  if (!input_filename) {
+    printf("Input file name is required.\n");
     exit(1);
   }
-  // @TODO free(doc)
-
-  hexDump("raw", out + MTSD_HEADER_SIZE, raw_size);
-
-  size_t compressed_size;
-  if (compress_data(out + MTSD_HEADER_SIZE, raw_size, &compressed_size) != 0) {
-    printf("compress_data ERR\n");
-    return 1;
+  if (!output_filename && encrypt) {
+    printf("Output file name is required. (use '-o' option)\n");
+    exit(1);
   }
-  memset(out + MTSD_HEADER_SIZE + compressed_size, 0x00, raw_size - compressed_size);
 
-  printf("(%zu - %zu) = saved: %zu B, ratio: %zu%\n", raw_size, compressed_size, raw_size - compressed_size, compressed_size * 100 / raw_size);
-  hexDump("lzma", out + MTSD_HEADER_SIZE, raw_size);
+  if (encrypt) {
+    cli_reader_state reader = { .read = 0 };
+    readfile(input_filename, &reader.content, &reader.size);
 
-  // encrypt(out + MTSD_HEADER_SIZE, compressed_size, "password", sizeof("password"), out + MTSD_HEADER_SIZE - MTSD_RANDOM_BYTES);
-  // hexDump("encrypted", out + MTSD_HEADER_SIZE, raw_size);
+    printf("Parsing file '%s' (%zu bytes)\n", input_filename, reader.size);
+    mtsd_document doc;
+    if (!mtsd_parse(read_handler, &reader, &doc)) {
+      printf("Cannot parse file\n");
+      exit(1);
+    }
+    free(reader.content);
 
-  uint8_t* normal;
-  decompress_data(out + MTSD_HEADER_SIZE, compressed_size, &normal, raw_size);
-  hexDump("restored", normal, raw_size);
+    printf("Encrypting %zu records\n", mtsd_doc_records_count(&doc));
+
+    char* password = getpass("Password: ");
+
+    uint8_t* encrypted_data = NULL;
+    size_t encrypted_size = 0;
+    if (!mtsd_encrypt(&doc, (uint8_t*)password, strlen(password), &encrypted_data, &encrypted_size)) {
+      printf("Cannot encrypt file\n");
+      exit(1);
+    }
+
+    printf("Writing %zu bytes to '%s'\n", encrypted_size, output_filename);
+    write_to_file(output_filename, encrypted_data, encrypted_size);
+
+    // hexDump("encrypted", encrypted_data, encrypted_size);
+  } else {
+    uint8_t* encrypted_data = NULL;
+    size_t encrypted_size = 0;
+    readfile(input_filename, &encrypted_data, &encrypted_size);
+
+    printf("Opening file '%s'\n", input_filename);
+
+    char* password = getpass("Password: ");
+
+    mtsd_document doc;
+    if (!mtsd_decrypt(encrypted_data, encrypted_size, (uint8_t*)password, strlen(password), &doc)) {
+      printf("Cannot decrypt file\n");
+      exit(1);
+    }
+    free(encrypted_data);
+
+    uint8_t* plaintext = NULL;
+    size_t text_size = 0;
+    if (!mtsd_to_text(&doc, &plaintext, &text_size)) {
+      printf("Cannot convert data to text\n");
+      exit(1);
+    }
+
+    if (output_filename) {
+      printf("Saving %zu records to '%s'\n", mtsd_doc_records_count(&doc), output_filename);
+      write_to_file(output_filename, plaintext, text_size);
+    } else {
+      printf("%.*s", text_size, plaintext);
+    }
+  }
 
   return 0;
 }
