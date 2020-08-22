2f3659b448d11211c69a9e56a40000b110d78df3
Alexander Drozdov <snosme@gmail.com>
2020-08-22 23:23:05 +0300

    document

--- include/mtsdata.h
+++ include/mtsdata.h
@@ -30,6 +30,7 @@ typedef enum {
   MTSD_EINCOMPRESSIBLE_DATA,
   MTSD_EENCODE_RECORD_SIZE,
   MTSD_EENCODE_PAYLOAD_SIZE,
+  MTSD_EDECODE_CORRUPTED_PAYLOAD,
 } mtsd_err;
 
 typedef struct mtsd_field {
@@ -64,9 +65,21 @@ mtsd_res mtsd_encrypt(/* In */ mtsd_document* doc,
                       /* Out */ size_t* size);
 
 mtsd_res mtsd_decrypt(/* In */ uint8_t* encrypted,
-                      /* In */ size_t* size,
+                      /* In */ size_t size,
                       /* In */ uint8_t* password,
                       /* In */ size_t password_len,
                       /* Out */ mtsd_document *doc);
+
+mtsd_res mtsd_to_text(/* In */ mtsd_document* doc,
+                      /* Out */ uint8_t** out,
+                      /* Out */ size_t* size);
+
+size_t mtsd_doc_records_count(const mtsd_document* doc);
+size_t mtsd_doc_record_fields_count(const mtsd_record* record);
+int mtsd_doc_field_is_multiline_value(const mtsd_field* field);
+inline void mtsd_doc_init(mtsd_document* doc);
+inline void mtsd_doc_record_init(mtsd_record* record);
+inline void mtsd_doc_field_init(mtsd_field* field);
+void mtsd_doc_free(mtsd_document* doc);
 
 #endif
---
+++ lib/document.c
@@ -0,0 +1,69 @@
+#include "mtsdata.h"
+#include <stdlib.h>
+
+size_t mtsd_doc_records_count(const mtsd_document* doc) {
+  size_t records_num = 0;
+  mtsd_record* record = doc->records;
+  while (record) {
+    records_num += 1;
+    record = record->next;
+  }
+  return records_num;
+}
+
+size_t mtsd_doc_record_fields_count(const mtsd_record* record) {
+  size_t fields_num = 0;
+  mtsd_field* field = record->fields;
+  while (field) {
+    fields_num += 1;
+    field = field->next;
+  }
+  return fields_num;
+}
+
+int mtsd_doc_field_is_multiline_value(const mtsd_field* field) {
+  for (size_t i = 0; i < field->value_size; i += 1) {
+    if (field->value[i] == '\n') {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void mtsd_doc_init(mtsd_document* doc) {
+  doc->records = NULL;
+}
+
+void mtsd_doc_record_init(mtsd_record* record) {
+  record->fields = NULL;
+  record->next = NULL;
+}
+
+void mtsd_doc_field_init(mtsd_field* field) {
+  field->key = 0;
+  field->value_size = 0;
+  field->value = NULL;
+  field->next = NULL;
+}
+
+void mtsd_doc_free(mtsd_document* doc) {
+  mtsd_record* record = doc->records;
+  while (record) {
+    mtsd_field* field = record->fields;
+    while (field) {
+      if (field->value) {
+        free(field->value);
+      }
+
+      mtsd_field* next = field->next;
+      free(field);
+      field = next;
+    }
+
+    mtsd_record* next = record->next;
+    free(record);
+    record = next;
+  }
+
+  mtsd_doc_init(doc);
+}
