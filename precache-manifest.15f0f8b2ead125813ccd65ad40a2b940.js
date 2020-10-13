cb39693d36939f63a163bdb3737e406be5d1383b
SnosMe <SnosMe@users.noreply.github.com>
2020-10-13 11:48:10 +0000

    deploy: 3c48453640db1ff33bf953b3866c34dc929d1afd

---
+++ precache-manifest.15f0f8b2ead125813ccd65ad40a2b940.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67da389594c8421fe80a",
+    "url": "/MyTopSecret-Data/css/app.b9905b16.css"
+  },
+  {
+    "revision": "fef94be5b7bec91f5b48c97b1285aabe",
+    "url": "/MyTopSecret-Data/fonts/InconsolataSemiExpanded-SemiBold.ttf"
+  },
+  {
+    "revision": "fc60fc5cc16c03fb409dbab22a28d996",
+    "url": "/MyTopSecret-Data/index.html"
+  },
+  {
+    "revision": "67da389594c8421fe80a",
+    "url": "/MyTopSecret-Data/js/app.cbedab4e.js"
+  },
+  {
+    "revision": "a386c1074680a5fda6f5",
+    "url": "/MyTopSecret-Data/js/chunk-vendors.5f60039f.js"
+  },
+  {
+    "revision": "3476e6fa205ba12315169d7f0e63bf23",
+    "url": "/MyTopSecret-Data/js/script.7e34cf0f.worker.js"
+  },
+  {
+    "revision": "7cdee792a2a1dc171b8ab5cf62e2523e",
+    "url": "/MyTopSecret-Data/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MyTopSecret-Data/robots.txt"
+  }
+]);
